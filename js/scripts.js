7f5acbb0f07cb46e269c7565717f4b588a5f668b
TNT <timrousdale@gmail.com>
2019-04-12 23:58:48 -0600

    Adds yardage/par calculation, adds IN/OUT calculations, adds better class managment

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-// MODAL
+// *****************************
+//            Modal
+// *****************************
 function show() {
     $('.add-player-modal').fadeIn(() => {
     })
@@ -9,126 +11,239 @@ function hide() {
     })
 }
 
+
 // *****************************
-//         HTTP Requests
+//           Classes
 // *****************************
 
-let GETAllCourses, GETCourse;
-
-GETAllCourses = new XMLHttpRequest();
-GETAllCourses.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-        let result = JSON.parse(this.responseText);
-        for (let i = 0; i < result.courses.length; i++) {
-            $('.course-select').append(`<option value="${result.courses[i].id}">${result.courses[i].name}</option>`);
-        }
-    }
-};
-
-(function () {
-    GETAllCourses.open("GET", `https://golf-courses-api.herokuapp.com/courses`, true);
-    GETAllCourses.send();
-})();
-
-
-GETCourse = new XMLHttpRequest();
-GETCourse.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-        let result = JSON.parse(this.responseText);
-        console.log(result);
-
-    }
-};
-
-function getCourse(id) {
-    if (id !== '0') {
-        GETCourse.open("GET", `https://golf-courses-api.herokuapp.com/courses/${id}`, true);
-        GETCourse.send();
-    }
-}
-
-
-let columns = 18;
-let players = 0;
-let game = {
-    course: '',
-    holes: 18,
-    teeOff: '',
-    players: []
-};
 
 class Course {
-    constructor(course, holes, tees) {
-        this.course = name;
-        this.holes = holes;
+    constructor() {
         this.players = [];
-    }
-    getCourse() {
-        //TODO Add http call here
-    }
+        this.numberOfPlayers = 0;
+        this.pars = [];
+        this.yardages = [];
+        this.data = {};
 
-
-}
-
-function addPlayer(name, handicap, tee) {
-    players++;
-    $('.score-column').append(`<div class="player-scores player-${players}-scores"></div>`);
-    // this.players.push(new Player(name, handicap, tee));
-
-    for (let i = 0; i < 18; i++) {
-        console.log('ran');
-
-
-        $(`.player-${players}-scores`).append(`<div class="score" id="${players}-${i}"><input class="score-input" type="number"></div>`);
     }
 
+    setParsAndYardage() {
+
+        //Inserts PAR Placeholder for the score inputs
+        if (this.data.holeCount && this.data.holes) {
+
+            for (let i = 0; i < this.players.length; i++) {
+                this.players[i].setPlayerParsAndYardage(this.data.holeCount, this.data.holes)
+            }
+        }
+
+    }
+
+    insOuts() {
+
+        for (let i = 0; i < this.players.length; i++) {
+
+            //Grabs holes 1-9 and calculates IN
+            let ins = 0;
+            for (let j = 0; j < 9; j++) {
+                let val = parseInt($(`#${i}-${j}`).children().val());
+                if (val) {
+                    ins += val;
+                }
+
+            }
+            $(`#${i}-in`).children().val(ins);
+
+            //Grabs holes 1-9 and calculates IN
+            let outs = 0;
+            for (let j = 9; j < 18; j++) {
+                let val = parseInt($(`#${i}-${j}`).children().val());
+                if (val) {
+                    outs += val;
+                }
+            }
+            $(`#${i}-out`).children().val(outs);
+
+        }
+    }
+
+    getCourse(id) {
+        if (id !== '0') {
+            fetch(`https://golf-courses-api.herokuapp.com/courses/${id}`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    this.data = data.data;
+                    this.pars = [];
+                    this.yardages = [];
+
+                    this.setParsAndYardage();
 
 
-    $(`.player-${players}-scores > div:nth-child(9)`).after(`<div class="score" id="${players}-in"><input readonly="true" class="score-input" type="number"></div>`);
+                    //TODO make this update overlay with course info
+                });
+        }
+    }
 
-    $(`.player-${players}-scores`).append(`<div class="score" id="${players}-out"><input readonly class="score-input" type="number"></div>`);
+    addPlayer(name, handicap, tee) {
 
-    $(".players").append(`<div class="player player-${players}" id="${players}">${name}</div>`);
 
-    $(".total").append(`<div class="player-total player-${players}" id="total-${players}"></div>`);
+        let playerID = this.numberOfPlayers;
+        this.players.push(new Player(name, handicap, tee, playerID));
+        this.players[playerID].createPlayerScorecard(name, handicap, tee, playerID);
+
+
+        this.numberOfPlayers++;
+        this.setParsAndYardage();
+
+    }
+
+    changePlayerName(name, id) {
+        for (let i = 0; i < this.players.length; i++) {
+            if (this.players[i].name === name) {
+                this.players[id].rename(name, true);
+                return;
+            }
+        }
+        this.players[id].rename(name, false);
+    }
 
 }
 
 
 class Player {
-    constructor(name, handicap, tee) {
+    constructor(name, handicap, tee, id) {
         this.name = name;
         this.handicap = handicap;
         this.tee = tee;
         this.scores = [];
+        this.total = 0;
+        this.pars = [];
+        this.yards = [];
         this.in = 0;
         this.out = 0;
+        this.id = id;
+    }
+
+    createPlayerScorecard() {
+
+        $('.score-column').append(`<div class="player-scores player-${this.id}-scores"></div>`);
+
+        for (let i = 0; i < 18; i++) {
+            //Makes 18 Score fields
+            $(`.player-${this.id}-scores`).append(`<div class="score" id="${this.id}-${i}-score"><input class="score-input" type="number" placeholder="par " onchange="game.players[${this.id}].score(${i}, this.value)"><div id="${this.id}-${i}-yards" class="yardage">Yards</div></div>`);
+        }
+
+        //Adds IN box
+        $(`.player-${this.id}-scores > div:nth-child(9)`).after(`<div class="score" id="${this.id}-in"><input readonly="true" class="score-input" type="number"></div>`);
+
+        //Adds OUT box
+        $(`.player-${this.id}-scores`).append(`<div class="score" id="${this.id}-out"><input readonly class="score-input" type="number"></div>`);
+
+        //Adds NAME box
+        $(".players").append(`<div class="player player-${this.id}" id="${this.id}" contenteditable onfocusout="game.changePlayerName($(this)[0].innerHTML, ${this.id})">${this.name}</div>`);
+
+        //Adds TOTAL box
+        $(".total").append(`<div class="player-total" id="${this.id}-total"></div>`);
+    }
+
+    setPlayerParsAndYardage(holeCount, holes) {
+        console.log(holes);
+        this.pars = [];
+        this.yards = [];
+        for (let i = 0; i < holeCount; i++) {
+            let hole = holes[i].teeBoxes;
+            let teeTypes = {
+                Pro: 0,
+                Champion: 0,
+                Men: 0,
+                Women: 0
+            };
+
+            for (let j = 0; j < hole.length; j++) {
+                teeTypes[hole[j].teeType] = j;
+            }
+
+            let par = hole[teeTypes[this.tee]].par;
+            let yards = hole[teeTypes[this.tee]].yards;
+            this.pars.push(par);
+            this.yards.push(yards);
+            $(`#${this.id}-${i}-score`).children().attr('placeholder', `par-${par}`);
+            $(`#${this.id}-${i}-yards`).html(`${yards} yards`);
+        }
+        console.log(this.yards);
 
     }
 
     score(hole, score) {
-        this.scores[hole] = score
+        this.scores[hole] = score;
+        this.setInOut();
+        this.setTotal();
     }
 
-    rename(name) {
-        this.name = name;
+    rename(name, nameTaken) {
+        if (nameTaken === true) {
+            $(`#${this.id}`).html(this.name);
+        } else {
+            this.name = name;
+            $(`#${this.id}`).html(name);
+        }
     }
-    getIn() {
-        let score = 0;
+
+    setInOut() {
+        this.in = 0;
         for (let i = 0; i < 9; i++) {
-            score += this.scores[i];
+            if (this.scores[i]) {
+                this.in += parseInt(this.scores[i]);
+            }
         }
-    }
-    getOut() {
-        let score = 0;
+        $(`#${this.id}-in`).children().val(this.in);
+
+        this.out = 0;
         for (let i = 9; i < 18; i++) {
-            score += this.scores[i];
+            if (this.scores[i]) {
+                this.out += parseInt(this.scores[i]);
+            }
         }
+        $(`#${this.id}-out`).children().val(this.out);
+
     }
+
+    setTotal() {
+        this.total = 0;
+        console.log('ran');
+        for (let i = 0; i < this.scores.length; i++) {
+            if (this.scores[i]) {
+                console.log('ran2');
+                this.total += (this.scores[i] - this.pars[i])
+            }
+        }
+        $(`#${this.id}-total`).html(this.total);
+    }
+
+
 }
 
-(function(el) {
 
+//Pro-1, champion- 2, men -3, women-4
+
+// *****************************
+//           MAIN JS
+// *****************************
+
+
+// INITIAL CALL
+(function () {
+    fetch('https://golf-courses-api.herokuapp.com/courses')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            for (let i = 0; i < data.courses.length; i++) {
+                $('.course-select').append(`<option value="${data.courses[i].id}">${data.courses[i].name}</option>`);
+            }
+        });
+
+    // Builds Hole Columns
     for (let i = 0; i < 18; i++) {
         let nth;
         switch (i) {
@@ -144,89 +259,39 @@ class Player {
             default:
                 nth = 'th';
         }
-        $(el).append(`<div class="hole" id="hole-${i}"><div class="hole-number">${i + 1}<span></span>${nth}</span></div></div>`);
+        $('.holes').append(`<div class="hole" id="hole-${i}"><div class="hole-number">${i + 1}<span></span>${nth}</span></div></div>`);
     }
-    $(".holes > div:nth-child(9)").after(`<div class="hole" id=""><div class="hole-number">In</span></div></div>`);
-    $(".holes").append(`<div class="hole" id=""><div class="hole-number">Out</span></div></div>`);
-})('.holes');
 
-// TODO Change this to an add player function, and make the UI only let one player add at a time
+    // Adds IN and OUT columns
+    $(".holes > div:nth-child(9)").after(`<div class="hole" id=""><div class="hole-number">IN</span></div></div>`);
+    $(".holes").append(`<div class="hole" id=""><div class="hole-number">OUT</span></div></div>`);
+})();
 
 
-// class Printer {
-//     constructor(ipAddress, brand, model) {
-//         this.ipAddress = ipAddress;
-//         this.brand = brand;
-//         this.model = model;
-//         this._status = 'Ready';
-//     };
-//
-//     print() {
-//         this._status = 'Printing'
-//     };
-//
-//     getStatus() {
-//         return this._status;
-//
-//     };
-//
-// }
-//
-// class InkJet extends Printer {
-//     constructor(ipAddress, brand, model, inkType) {
-//         super(ipAddress, brand, model);
-//         this.inkType = inkType
-//
-//     }
-//
-//     print() {
-//         // do stuff
-//     }
-// }
-//
-// class Ink {
-//     constructor() {
-//         this.magenta = 100;
-//         this.cyan = 100;
-//         this.yellow = 100;
-//     }
-// }
-//
-// const inky = new InkJet('10.0.0.8', 'hp', 'xr-121', new Ink());
-// const printer = new Printer('168.192.10.3', 'Brother', 'MFC');
-//
-// console.log(printer.getStatus());
-// console.log(inky);
-//
-// // Callback
-//
-// function getData(id, callback) {
-//     let data = {name: 'secret angent'};
-//     callback(data);
-// }
-//
-// getData(232323, (data) => {
-//     // do something with data
-//     console.log(data);
-// });
-//
-// // Promise
-//
-// function getDataPromise(id) {
-//     return new Promise((resolve, reject) => {
-//         if (!id) {
-//             reject({error: 'you are smart', message: 'try again'});
-//         }
-//         let data = {name: 'secret angent'};
-//         resolve(data);
-//     })
-// }
-//
-// let dataPromise = getDataPromise(23423);
-// dataPromise.then(data => {
-//         console.log(`from .the ${data}`);
-//     });
+let game = new Course();
+
+
+//TODO DELETE this placeholder info
+let b = {
+    addr1: "1400 N 200 E",
+    addr2: null,
+    city: "American Fork",
+    country: "United States",
+    courseId: 18300,
+    holeCount: 18,
+    holes: [],
+    id: "18300",
+    lat: 40.4031413225741,
+    lng: -111.787138581276,
+    name: "Fox Hollow Golf Club",
+    phone: "(801) 756-3594",
+    stateOrProvince: "UT",
+    thumbnail: "https://swingbyswing-b9.s3.amazonaws.com/photo/in-round/12486769/uploaded-photo43828077-480x360.png",
+    website: "http://www.foxhollowutah.com/",
+    zipCode: "84003",
+};
 
 
 // TODO Remove this, it's for testing
-addPlayer('tim', 2, 'white');
+game.addPlayer('tim', 2, 'Champion');
+game.addPlayer('timmy', 2, 'Men');
